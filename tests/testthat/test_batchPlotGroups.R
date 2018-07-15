b5d528dfb3b80db345da5a0d18e7a59d43a91057
Thomas Weise <tweise@hfuu.edu.cn>
2018-07-16 07:14:08 +0800

    OK, sourcing of tests does not work

--- tests/testthat/test_batchPlotGroups.R
+++ tests/testthat/test_batchPlotGroups.R
@@ -12,7 +12,44 @@ test_that("Test batch-plotting of groups", {
 
   pdf(dest, width=6, height=3);
 
-  source("../../examples/batchPlotGroups.R");
+  # set a random seed for replicability
+  set.seed(2677);
+
+  # the three base functions with the mean parameter values
+  f <- list(
+    list(f=function(x, par) par[1] + par[2]*x + par[3]*x*x + par[4]*x*x*x,
+         m=c(1, -0.2, 0.75, -0.3)),
+    list(f = function(x, par) par[1] * exp(par[2] - x),
+         m=c(0.1, 3)),
+    list(f=function(x, par) par[1] + par[2]*sin(par[3]*x),
+         m=c(0, 1, 3)));
+
+  # create the three example data sets
+  examples <- lapply(X=f, FUN=function(example) {
+    # for each example function, plot 4 to 50 instances
+    lapply(seq_len(runif(n=1, min=4, max=50)),
+           # for each instance
+           FUN=function(i) {
+             # randomly choose the x-coordinates
+             x <- runif(n=as.integer(round(runif(n=1, min=10, max=200))),
+                        min=0, max=3);
+             m <- example$m;
+             # pick parameters which are normally distributed around the suggestion
+             par <- rnorm(n=length(m), mean=m, s=0.1*abs(m));
+             # and construct a function
+             fff <- function(x) example$f(x, par);
+             # and pass this function as result example together with the x values
+             list(x=x, f=fff)
+           })
+  });
+
+  # plot the original data
+  batchPlot.groups(examples,
+                   names=c("f1", "f2", "f3"),
+                   main="Several Groups of Functions",
+                   plotXY=FALSE, plotXF=TRUE,
+                   legend=list(x="bottom", horiz=TRUE));
+
 
   dev.off();
 
--- tests/testthat/test_plotColors.R
+++ tests/testthat/test_plotColors.R
@@ -12,10 +12,26 @@ test_that("Test plotting the colors", {
 
   pdf(file=dest, width=6, height=6);
 
-  x.range <- 4L;
-  y.range <- 4L;
+  # set the random seed for reproducibility
+  set.seed(129855L);
 
-  source("../../examples/plotColors.R", local=environment());
+  x.range <- 4L;
+  y.range <- 5L;
+
+  # set the number of diagrams appropriately
+  old.par <- par(mfrow=c(y.range, x.range), mai=c(0.02, 0.02, 0.02, 0.02));
+
+  # paint the diagrams
+  n <- 1L;
+  for(y in 1L:y.range) {
+    for(x in 1L:x.range) {
+      plot.colors(colors.distinct(n));
+      n <- n + 1L;
+    }
+  }
+
+  # reset the parameters
+  invisible(par(old.par));
 
   dev.off();
   graphics.off();
