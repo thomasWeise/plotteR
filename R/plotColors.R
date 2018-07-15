69db2fb01e9bb835ba758989b21e69ac60ce8d00
Thomas Weise <tweise@home>
2018-07-15 09:15:19 +0800

    Improved Color Plotting

--- R/plotColors.R
+++ R/plotColors.R
@@ -4,7 +4,7 @@
 #'   and distinct.
 #' @param x the colors to plot
 #' @param ... the parameters passed to \code{pie} or \code{plot}
-#' @importFrom graphics pie plot polygon
+#' @importFrom graphics pie plot rect
 #' @export plot.colors
 plot.colors <- function(x, ...) {
   x <- unlist(x, recursive = TRUE);
@@ -20,36 +20,91 @@ plot.colors <- function(x, ...) {
                            FUN=function(i) {
                              lapply(X=(i+1L):n,
                                     FUN=function(j, i) c(i, j), i)
-                           }), recursive = FALSE);
-    pairs.n <- length(pairs);
+                           }), recursive = TRUE);
 
-    n.x <- as.integer(ceiling(sqrt(pairs.n)) + 0.1);
-    n.y <- as.integer(ceiling(pairs.n / n.x) + 0.1);
+    n <- length(pairs);
+    repeat {
+      if(n <= 2L) { break; }
 
-    plot(x=c(0,n.x), y=c(0,n.y), type="n", xlab="", ylab="", ann=FALSE,
-         asp=TRUE, bty="n", xaxt="n", yaxt="n", ...);
+      pairs <- pairs[vapply(X=1L:n,
+                      FUN=function(i) {
+                        if(i <= 1L) { return(TRUE); }
+                        else { return(pairs[i] != pairs[i-1L]); }
+                      }, FUN.VALUE = FALSE)];
 
-    i <- 1L;
-    for(b in seq_len(n.y)) {
-      for(a in seq_len(n.x)) {
-        if(i <= pairs.n) {
-          pair <- pairs[[i]];
-          x2 <- a;
-          x1 <- (x2 - 1L);
-          y2 <- n.y - b;
-          y1 <- y2 + 1L;
+      n <- length(pairs);
+      if(n <= 2L) { break; }
 
-          polygon(x=c(x1, x2, x2),
-                  y=c(y1, y1, y2),
-                  col=x[pair[1L]],
-                  lty=c(0,0));
-          polygon(x=c(x1, x1, x2),
-                  y=c(y1, y2, y2),
-                  col=x[pair[2L]],
-                  lty=c(0,0));
-          i <- i + 1L;
+      for(i in 2L:(n-1L)) {
+        A <- pairs[i - 1L];
+        B <- pairs[i];
+        C <- pairs[i + 1L];
+
+        if(A == C) {
+          skip <- FALSE;
+          for(j in 1L:(n-1L)) {
+            if(j != i)  {
+              if((pairs[j] == B) && (pairs[j + 1L] == C)) {
+                pairs <- pairs[-i];
+                pairs <- pairs[-i];
+                skip <- TRUE;
+                break;
+              }
+            }
+          }
+          if(skip) { break; }
+
+          if(A == C) {
+            for(j in 2L:n) {
+              if(j != i)  {
+                if((pairs[j] == B) && (pairs[j - 1L] == A)) {
+                  pairs <- pairs[-i];
+                  pairs <- pairs[-i];
+                  skip <- TRUE;
+                  break;
+                }
+              }
+            }
+          }
+          if(skip) { break; }
+        }
+
+        foundAB <- FALSE;
+        foundBC <- FALSE;
+        for(j in 2L:n) {
+          if(j != i) {
+            if( ((B == pairs[j]) && (A == pairs[j - 1L])) ) {
+              foundAB <- TRUE;
+              break;
+            }
+          }
+        }
+
+        for(j in 1L:(n-1L)) {
+          if(j != i) {
+            if( ((B == pairs[j]) && (C == pairs[j + 1L])) ) {
+              foundBC <- TRUE;
+              break;
+            }
+          }
+        }
+
+        if(foundAB && foundBC) {
+          pairs <- pairs[-j];
+          break;
         }
       }
+
+      n.old <- n;
+      n     <- length(pairs);
+      if(n >= n.old) { break; }
+    }
+
+    plot(x=c(0,n), y=c(0,1), type="n", xlab="", ylab="", ann=FALSE,
+         asp=FALSE, bty="n", xaxt="n", yaxt="n", ...);
+
+    for(i in 1L:n){
+      rect((i-1L), 1, i, 0, col=x[pairs[i]], border=NA);
     }
   }
 }
